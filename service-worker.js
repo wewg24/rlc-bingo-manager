f0da2c3e5fc96b7e8988b9975a7be1f1d7b3dbcb
wewg24 <84467156+wewg24@users.noreply.github.com>
2025-09-11 22:31:35 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
 
+// Caching strategy
+const CACHE_STRATEGY = {
+  static: 'cache-first',      // CSS, JS, images
+  api: 'network-first',        // API calls
+  photos: 'cache-then-network', // User photos
+  reports: 'cache-first'        // Generated PDFs
+};
